01093e3cceefc8662b4d1e277f7033bff87ef850
Emerson Saturnino <emerson.saturnino@finnet.com.br>
2022-01-20 22:26:25 -0300

    feat: Added import organization plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import App from './App';
 import theme from './styles/theme';
 
 ReactDOM.render(
